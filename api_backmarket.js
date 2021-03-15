3fd3059f26c3963ef41427fa98d4678b60ed8c2a
adityawisnuf <adityaferdiansyah33@gmail.com>
2021-03-15 17:48:13 +0700

    add API key to API

--- api_backmarket.js
+++ api_backmarket.js
@@ -7,38 +7,64 @@ db.connect(function(err){
    if (err) throw err;
 
 app.get('/api', function (req, res){
-    var id = 'AND id LIKE \'%'+req.param('id')+'%\'';
-    var bmId = 'AND backmarket_id LIKE \'%'+req.param('backmarket_id')+'%\'';
-    var title = req.param('title')
-    title = title != undefined?title.replace(/ /g, '%'):title
-    title = 'AND title LIKE \'%'+title+'%\'';
-    var marketplace_cat_id = 'AND marketplace_category_id LIKE \'%'+req.param('marketplace_category_id')+'%\''
-    var getDataSQL = `SELECT * FROM backmarket_product WHERE 1 ${id} ${bmId} ${title} ${marketplace_cat_id}`;
-    db.query(getDataSQL.replace(/undefined/g, ''), async function(err, result){
-        var data = {
-            rows : result.length,
-            results : result
+    var key = req.param('key');
+    if(key == undefined){
+        var errornya = {
+            message : "Missing Authentication Token"
         }
-        res.send(data)
-    });
+        res.json(errornya)
+    }else if(key === '150321'){
+        var id = 'AND id LIKE \'%'+req.param('id')+'%\'';
+        var bmId = 'AND backmarket_id LIKE \'%'+req.param('backmarket_id')+'%\'';
+        var title = req.param('title')
+        title = title != undefined?title.replace(/ /g, '%'):title
+        title = 'AND title LIKE \'%'+title+'%\'';
+        var marketplace_cat_id = 'AND marketplace_category_id LIKE \'%'+req.param('marketplace_category_id')+'%\''
+        var getDataSQL = `SELECT * FROM backmarket_product WHERE 1 ${id} ${bmId} ${title} ${marketplace_cat_id}`;
+        db.query(getDataSQL.replace(/undefined/g, ''), async function(err, result){
+            var data = {
+                rows : result.length,
+                results : result
+            }
+            res.send(data)
+        });
+    }else{
+        var errornya = {
+            message : "Incorrect Authentication Token"
+        }
+        res.json(errornya)
+    }
 })
 
 app.get('/api/:category', function (req, res){
-    var Category = 'AND category_name LIKE \'%'+req.params.category+'%\'';
-    var id = 'AND id LIKE \'%'+req.param('id')+'%\'';
-    var bmId = 'AND backmarket_id LIKE \'%'+req.param('backmarket_id')+'%\'';
-    var title = req.param('title')
-    title = title != undefined?title.replace(/ /g, '%'):title
-    title = 'AND title LIKE \'%'+title+'%\'';
-    var marketplace_cat_id = 'AND marketplace_category_id LIKE \'%'+req.param('marketplace_category_id')+'%\''
-    var getDataSQL = `SELECT * FROM backmarket_product WHERE 1 ${Category} ${id} ${bmId} ${title} ${marketplace_cat_id}`;
-    db.query(getDataSQL.replace(/undefined/g, ''), async function(err, result){
-        var data = {
-            rows : result.length,
-            results : result
+    var key = req.param('key');
+    if(key == undefined){
+        var errornya = {
+            message : "Missing Authentication Token"
         }
-        res.send(data)
-    });
+        res.json(errornya)
+    }else if(key === '150321'){
+        var Category = 'AND category_name LIKE \'%'+req.params.category+'%\'';
+        var id = 'AND id LIKE \'%'+req.param('id')+'%\'';
+        var bmId = 'AND backmarket_id LIKE \'%'+req.param('backmarket_id')+'%\'';
+        var title = req.param('title')
+        title = title != undefined?title.replace(/ /g, '%'):title
+        title = 'AND title LIKE \'%'+title+'%\'';
+        var marketplace_cat_id = 'AND marketplace_category_id LIKE \'%'+req.param('marketplace_category_id')+'%\''
+        var getDataSQL = `SELECT * FROM backmarket_product WHERE 1 ${Category} ${id} ${bmId} ${title} ${marketplace_cat_id}`;
+        db.query(getDataSQL.replace(/undefined/g, ''), async function(err, result){
+            var data = {
+                rows : result.length,
+                results : result
+            }
+            res.send(data)
+        });
+    }else{
+        var errornya = {
+            message : "Incorrect Authentication Token"
+        }
+        res.json(errornya)
+    }
 })
 
 app.listen(6464)
